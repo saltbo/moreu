9fa2ecedfff218db979d9cc997cc8adc5b945a53
saltbo <saltbo@foxmail.com>
2020-08-23 18:23:07 +0800

    refactor: update the model delete for the gorm

--- model/user.go
+++ model/user.go
@@ -12,15 +12,15 @@ const (
 )
 
 type User struct {
-	ID        int64     `json:"id"`
-	Email     string    `json:"email" gorm:"size:32;unique_index;not null"`
-	Username  string    `json:"username" gorm:"size:20;unique_index;not null"`
-	Password  string    `json:"-" gorm:"size:32;not null"`
-	Roles     string    `json:"roles" gorm:"size:64;not null"`
-	Activated bool      `json:"activated" gorm:"not null"`
-	Deleted   time.Time `json:"deleted" gorm:"column:deleted_at;not null"`
-	Created   time.Time `json:"created" gorm:"column:created_at;not null"`
-	Updated   time.Time `json:"updated" gorm:"column:updated_at;not null"`
+	ID        int64      `json:"id"`
+	Email     string     `json:"email" gorm:"size:32;unique_index;not null"`
+	Username  string     `json:"username" gorm:"size:20;unique_index;not null"`
+	Password  string     `json:"-" gorm:"size:32;not null"`
+	Roles     string     `json:"roles" gorm:"size:64;not null"`
+	Activated bool       `json:"activated" gorm:"not null"`
+	Deleted   *time.Time `json:"-" gorm:"column:deleted_at"`
+	Created   time.Time  `json:"created" gorm:"column:created_at;not null"`
+	Updated   time.Time  `json:"updated" gorm:"column:updated_at;not null"`
 }
 
 func (User) TableName() string {
--- model/user_profile.go
+++ model/user_profile.go
@@ -3,17 +3,17 @@ package model
 import "time"
 
 type UserProfile struct {
-	Id       int64     `json:"id"`
-	UserId   int64     `json:"user_id" gorm:"not null"`
-	Nickname string    `json:"nickname" gorm:"size:32;not null"`
-	Avatar   string    `json:"avatar" gorm:"size:255;not null"`
-	Bio      string    `json:"bio" gorm:"size:255;not null"`
-	URL      string    `json:"url" gorm:"size:255;not null"`
-	Company  string    `json:"company" gorm:"size:32;not null"`
-	Location string    `json:"location" gorm:"size:32;not null"`
-	Deleted  time.Time `json:"deleted" gorm:"column:deleted_at;not null"`
-	Created  time.Time `json:"created" gorm:"column:created_at;not null"`
-	Updated  time.Time `json:"updated" gorm:"column:updated_at;not null"`
+	Id       int64      `json:"id"`
+	UserId   int64      `json:"user_id" gorm:"not null"`
+	Nickname string     `json:"nickname" gorm:"size:32;not null"`
+	Avatar   string     `json:"avatar" gorm:"size:255;not null"`
+	Bio      string     `json:"bio" gorm:"size:255;not null"`
+	URL      string     `json:"url" gorm:"size:255;not null"`
+	Company  string     `json:"company" gorm:"size:32;not null"`
+	Location string     `json:"location" gorm:"size:32;not null"`
+	Deleted  *time.Time `json:"-" gorm:"column:deleted_at"`
+	Created  time.Time  `json:"created" gorm:"column:created_at;not null"`
+	Updated  time.Time  `json:"updated" gorm:"column:updated_at;not null"`
 }
 
 func (UserProfile) TableName() string {
